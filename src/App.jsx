9964f2e030ac652370fc43f3e88262cead31df36
Imran Manzoor <57487946+imanzoor@users.noreply.github.com>
2024-06-25 17:15:38 +0000

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Imran Manzoor",
+  title: "Altium Certified PCB Designer",
+  email: "imanzoor.mts22ceme@student.nust.edu.pk",
+  gitHub: "imanzoor",
+  instagram: "",
+  linkedIn: "imran-manzoor-51275227/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/mtu.png";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Altium Designer - Essentials",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Altium approved program of study and assessment.",
+    url: "https://certificates.altium.com/?Id=535a94b7-5fcf-191b-7d59-fbbc87120e8c",
   },
   {
-    title: "Web Development for Beginners",
+    title: "High Speed Design Services",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Contributed to startups by providing, high tech designs..",
+    url: "https://www.linkedin.com/services/page/7146943248abb21aa2/",
   },
   {
-    title: "My Resume Site",
+    title: "Global Services",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Provides High end solutions accross the globe",
+    url: "https://www.fiverr.com/imranmanzoor15?up_rollout=true",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "GitHub",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Open Source Projects.",
+    url: "https://github.com/imanzoor",
   },
 ];
 
